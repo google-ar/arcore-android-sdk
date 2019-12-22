16bbdb95aae17b719ade9375184c988c1da66252
Hannes Achleitner <hannes.software@gmx.at>
2019-12-22 08:47:33 +0100

    Android Studio 3.5.3 and Android X

--- samples/cloud_anchor_java/app/src/main/java/com/google/ar/core/examples/java/common/helpers/SnackbarHelper.java
+++ samples/cloud_anchor_java/app/src/main/java/com/google/ar/core/examples/java/common/helpers/SnackbarHelper.java
@@ -15,8 +15,10 @@
 package com.google.ar.core.examples.java.common.helpers;
 
 import android.app.Activity;
-import android.support.design.widget.BaseTransientBottomBar;
-import android.support.design.widget.Snackbar;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.ar.core.examples.java.cloudanchor.R;
+
 import android.view.View;
 import android.widget.TextView;
 
@@ -115,7 +117,7 @@ public final class SnackbarHelper {
             ((TextView)
                     messageSnackbar
                         .getView()
-                        .findViewById(android.support.design.R.id.snackbar_text))
+                        .findViewById(R.id.snackbar_text))
                 .setMaxLines(maxLines);
             messageSnackbar.show();
           }
