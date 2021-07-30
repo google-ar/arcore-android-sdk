85a5418204aa80f8891997243499bf161bd52581
Hannes Achleitner <hannes.software@gmx.at>
2021-07-30 15:07:41 +0200

    Decrypt google json

--- signing/decrypt.sh
+++ signing/decrypt.sh
@@ -14,15 +14,21 @@ else
    echo "\$CRYPT_PASS available"
 fi
 
-cd signing
+pushd signing
 
 # to encrypt
-#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in release.keystore -out release.keystore.enc
-#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in Surveilance-playstore.json -out Surveilance-playstore.json.enc
+#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -pbkdf2 -iter 100000 -in release.keystore -out release.keystore.enc
+#openssl aes-256-cbc -salt -pbkdf2 -k "$CRYPT_PASS" -in all-in-one-sample/google-services.json -out all-in-one-sample/google-services.json.enc
 
 # Ubuntu 18.04 (openssl 1.1.0g+) needs -md md5
 # https://askubuntu.com/questions/1067762/unable-to-decrypt-text-files-with-openssl-on-ubuntu-18-04/1076708
+echo release.keystore
+echo ================
 openssl aes-256-cbc -a -d -md md5 -k "$CRYPT_PASS" -in release.keystore.enc -out release.keystore
-#openssl aes-256-cbc -a -d -md md5 -k "$CRYPT_PASS" -in Surveilance-playstore.json.enc -out Surveilance-playstore.json
-
-cd ..
+echo google-services.json
+echo ====================
+set +x
+openssl aes-256-cbc -d -pbkdf2 -k "$CRYPT_PASS" -in ../all-in-one-sample/google-services.json.enc -out ../all-in-one-sample/google-services.json
+find . -name "google-services.json"
+set -x
+popd 1>/dev/null
